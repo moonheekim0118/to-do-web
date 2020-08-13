1a94c799dc17229d9ca483c0616f7db70b9f0981
moonee <uuuu118@naver.com>
2020-08-13 20:03:40 +0900

    Edit nav bar style

--- public/js/originSource/nav.js
+++ public/js/originSource/nav.js
@@ -1,4 +1,18 @@
+const nav_bar= document.querySelector('nav');
+const close_btn =document.getElementById('nav-toggle');
 const nav_btn = document.getElementById('toggle');
+
 nav_btn.addEventListener('click', ()=>{
-    document.body.classList.toggle('show-nav');
+    nav_bar.classList.add('show-nav');
+})
+
+close_btn.addEventListener('click', ()=>{
+    nav_bar.classList.remove('show-nav');
+})
+
+window.addEventListener('click', (e)=>{
+    console.log(e.target);
+    if(e.target.className ==='show-nav'){
+        nav_bar.classList.remove('show-nav');
+    } 
 })
